650fc56ebac3bc469d2f3a57d80c4ca28884a6e7
Alex Conrey <alex@conrey.us>
2018-07-16 13:20:26 -0500

    refactoring types

---
+++ lib/puppet/provider/prometheus_host/parsed.rb
@@ -0,0 +1,25 @@
+require 'puppet/provider/parsedfile'
+
+Puppet::Type.type(:prometheus_houst).provide(
+  :parsed,
+  :parent => Puppet::Provider::ParsedFile,
+  :filetype => :flat
+) do
+  desc "Parse and generate JSON files for prometheus based on prometheus_host exporters."
+
+  record_line :parsed, :fields => %w{host_name labels port},
+    :post_parse => proc { |hash|
+      puts hash
+    },
+    :pre_gen => proc { |hash|
+      puts hash
+    }
+
+  def self.default_mode
+    0644
+  end
+
+  def self.default_target
+    "/etc/prometheus/prometheus_host.json"
+  end
+end
--- lib/puppet/type/prometheus_host.rb
+++ lib/puppet/type/prometheus_host.rb
@@ -2,38 +2,44 @@ require_relative 'prometheus'
 require_relative 'prometheus/base'
 require_relative '../provider/prominator'
 
-
-Puppet::Type.newtype(:prometheus_host) do
-  @doc = "Manage prometheus_host resources"
+module Puppet
+  Type.newtype(:prometheus_host) do
+    @doc = "Manage prometheus_host resources"
   
-  ensurable
+    ensurable
 
-  newparam(:name) do
-    isnamevar
-    desc "Exporter name"
+    newparam(:name) do
+      isnamevar
+      desc "Exporter name"
 
-    validate do |value|
-      raise Puppet::Error,_("Resource name cannot include whitespaces") if value =~ /\s/
-    end 
-  end
+      validate do |value|
+        raise Puppet::Error,_("Resource name cannot include whitespaces") if value =~ /\s/
+      end 
+    end
 
-  newproperty(:host_name) do
-    desc "Host name of the exporter"
-  end
+    newproperty(:host_name) do
+      desc "Host name of the exporter"
+    end
 
-  newproperty(:labels) do
-    desc "Hash of labels to apply to the exporter"
-  end
+    newproperty(:labels) do
+      desc "Hash of labels to apply to the exporter"
+    end
 
-  newproperty(:port) do
-    desc "Port for the exporter to connect to"
-  end
+    newproperty(:port) do
+      desc "Port for the exporter to connect to"
+    end
   
-  # File path to place the resource information in
-  newproperty(:target) do
-    desc "The file in which to source the exporter information. Only used by
+    # File path to place the resource information in
+    newproperty(:target) do
+      desc "The file in which to source the exporter information. Only used by
         the `parsed` provider."
-
-    defaultto "/etc/prometheus/prometheus_host.json"
+    
+      defaultto { if @resource.class.defaultprovider.ancestors.include?(Puppet::Provider::ParsedFile)
+        @resource.class.defaultprovider.default_target
+        else 
+          nil
+        end
+      }
+    end
   end
 end
